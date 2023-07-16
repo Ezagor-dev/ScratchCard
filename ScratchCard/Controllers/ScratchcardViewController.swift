16f653501f0426c208775a0dc9dcd859c4cf5675
Ata <atakanozalan@gmail.com>
2023-07-16 04:17:35 +0300

    creating ScratchcardViewController

---
+++ ScratchCard/Controllers/ScratchcardViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ScratchcardViewController.swift
+//  ScratchCard
+//
+//  Created by Ezagor on 16.07.2023.
+//
+
+import Foundation
+import UIKit
+
+class ScratchcardViewController: UIViewController {
+    private var scratchcardView: ScratchcardView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        scratchcardView = ScratchcardView(frame: view.bounds)
+        view.addSubview(scratchcardView)
+    }
+}
